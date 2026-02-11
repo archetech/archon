be243e99db6b73489eea00f17cc512e719b78d69
David McFadzean <davidmc@gmail.com>
2026-02-11 13:30:59 -0500

    Fixed message

--- apps/react-wallet/src/components/WalletTab.tsx
+++ apps/react-wallet/src/components/WalletTab.tsx
@@ -87,7 +87,7 @@ const WalletTab = () => {
             const { idsRemoved, ownedRemoved, heldRemoved, aliasesRemoved } =
                 await keymaster.fixWallet();
             setSuccess(
-                `${idsRemoved} IDs removed\n${ownedRemoved} owned DIDs removed\n${heldRemoved} held DIDs removed\n${aliasesRemoved} names removed`
+                `${idsRemoved} IDs removed\n${ownedRemoved} owned DIDs removed\n${heldRemoved} held DIDs removed\n${aliasesRemoved} aliases removed`
             );
         } catch (error: any) {
             setError(error);
