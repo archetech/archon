34d743c9f8e795440ee0c467ee1841ba435f80ff
David McFadzean <davidmc@gmail.com>
2026-02-12 14:54:04 -0500

    chore: Improve security (#65)
    * Initial audit
    
    * Locked down signet

--- services/gatekeeper/server/src/config.js
+++ services/gatekeeper/server/src/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const config = {
     port: process.env.ARCHON_GATEKEEPER_PORT ? parseInt(process.env.ARCHON_GATEKEEPER_PORT) : 4224,
+    bindAddress: process.env.ARCHON_BIND_ADDRESS || '0.0.0.0',
     db: process.env.ARCHON_GATEKEEPER_DB || 'redis',
     ipfsURL: process.env.ARCHON_IPFS_URL || 'http://localhost:5001/api/v0',
     didPrefix: process.env.ARCHON_GATEKEEPER_DID_PREFIX || 'did:cid',
@@ -11,6 +12,7 @@ const config = {
     jsonLimit: process.env.ARCHON_GATEKEEPER_JSON_LIMIT || '4mb',
     gcInterval: process.env.ARCHON_GATEKEEPER_GC_INTERVAL ? parseInt(process.env.ARCHON_GATEKEEPER_GC_INTERVAL) : 15,
     statusInterval: process.env.ARCHON_GATEKEEPER_STATUS_INTERVAL ? parseInt(process.env.ARCHON_GATEKEEPER_STATUS_INTERVAL) : 5,
+    adminApiKey: process.env.ARCHON_ADMIN_API_KEY || '',
 };
 
 export default config;
--- services/keymaster/server/src/config.js
+++ services/keymaster/server/src/config.js
@@ -5,11 +5,13 @@ dotenv.config();
 const config = {
     gatekeeperURL: process.env.ARCHON_GATEKEEPER_URL || 'http://localhost:4224',
     keymasterPort: process.env.ARCHON_KEYMASTER_PORT ? parseInt(process.env.ARCHON_KEYMASTER_PORT) : 4226,
+    bindAddress: process.env.ARCHON_BIND_ADDRESS || '0.0.0.0',
     nodeID: process.env.ARCHON_NODE_ID || '',
     db: process.env.ARCHON_KEYMASTER_DB || 'json',
     keymasterPassphrase: process.env.ARCHON_ENCRYPTED_PASSPHRASE || '',
     walletCache: process.env.ARCHON_WALLET_CACHE ? process.env.ARCHON_WALLET_CACHE === 'true' : false,
-    defaultRegistry: process.env.ARCHON_DEFAULT_REGISTRY
+    defaultRegistry: process.env.ARCHON_DEFAULT_REGISTRY,
+    adminApiKey: process.env.ARCHON_ADMIN_API_KEY || '',
 };
 
 export default config;
