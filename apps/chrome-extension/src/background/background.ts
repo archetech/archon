d882a02105792d4b1fc2e4afb4caa673de93c499
flaxscrip <flaxscrip@archon.dzo-hops.ts.net>
2026-03-02 22:27:23 +0000

    Fix "A" icons for apps and wallet

--- apps/chrome-extension/src/background/background.ts
+++ apps/chrome-extension/src/background/background.ts
@@ -1,6 +1,6 @@
 import {openBrowserValues} from "../contexts/UIContext";
 
-const DEFAULT_GATEKEEPER_URL = "http://localhost:4224";
+const DEFAULT_GATEKEEPER_URL = "https://archon.technology";
 
 async function ensureDefaultSettings() {
     try {
