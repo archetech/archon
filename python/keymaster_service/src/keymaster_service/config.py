064bd7abc43c9f04e0b4c4165e9d0a67daab53d4
David McFadzean <davidmc@gmail.com>
2026-05-31 13:38:26 -0400

    chore: Init v0.10.0 (#588)
    * Bump app and service versions to v0.10.0
    
    * Include Python runtime metadata in v0.10.0 bump

--- python/keymaster_service/src/keymaster_service/config.py
+++ python/keymaster_service/src/keymaster_service/config.py
@@ -9,7 +9,7 @@ def _package_version() -> str:
     try:
         return metadata.version("keymaster-service")
     except metadata.PackageNotFoundError:
-        return "0.9.0"
+        return "0.10.0"
 
 
 @dataclass(slots=True)
